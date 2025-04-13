c095e96b0bc332a82b5cfac74282587f12fae0ce
SC <princessstevm@users.noreply.github.com>
2025-04-13 11:42:58 +1000

    Initial

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -10,8 +11,17 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
 
 class Choice(models.Model):
     
--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -3,15 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 
-from .models import Question
+from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import render
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
+""" def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     #    #output = ", ".join([q.question_text for q in latest_question_list])
@@ -39,7 +41,27 @@ def results(request, question_id):
     #response = "You're looking at the results of question %s."
     #return HttpResponse(response % question_id)
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+    return render(request, "polls/results.html", {"question": question}) """
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        #return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    #exclude questions not published yet
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     #return HttpResponse("You're voting on question %s." % question_id)
